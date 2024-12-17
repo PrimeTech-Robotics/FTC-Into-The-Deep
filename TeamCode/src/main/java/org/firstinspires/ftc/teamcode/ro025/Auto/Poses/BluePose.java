b6c413a7a6bd1ecd27a5740d5ca03a5d6e8423a1
Robert Lupas <robertlupas09@gmail.com>
2024-12-18 00:13:10 +0200

    Create Poses class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ro025/Auto/Poses/BluePose.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.ro025.Auto.Poses;
+
+import static org.firstinspires.ftc.teamcode.ro025.Auto.Poses.PoseName.*;
+
+public class BluePose extends Pose {
+
+    public BluePose() {
+        setPose(START_POSE, 17, 61.85, 90);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ro025/Auto/Poses/PoseName.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.ro025.Auto.Poses;
+
+public enum PoseName {
+    START_POSE
+}
